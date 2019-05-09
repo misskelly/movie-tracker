f9b9b3ad4c36752f917cb94254b2e5418af10905
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-09 17:25:21 -0600

    Allow logged in user to log out

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'; 
-import { formType }  from '../../actions/index'
-
+import { formType, currentUser }  from '../../actions/index'
 
 export class Header extends Component {
-  constructor() {
-    super()
-  }
-
   handleClick = ({target}) => {
-    target.textContent === 'Log In' 
+    target.textContent === 'Log-in' 
       ? this.props.formType('login')
       : this.props.formType('signup')
+    target.textContent === 'Log-out' && this.props.logOutUser()
   }
 
   render() {
     const { name, email } = this.props.currentUser;
+    const userInfo = (
+      <div>
+        <h3>{`Yo watup ${name}`}</h3>
+      </div>
+    )
     return(
       <header className='header'>
         <NavLink to='/' className='nav'> Home </NavLink>
-        {/* { name ? (<div></div>)} */}
+        { name && userInfo}
         <NavLink to={`${name ? '/favorites' : '/login'}`} className='nav'>
           <span onClick={this.handleClick}>{`${ name ? 'Favorites' : 'Create Account'}`}
           </span>
         </NavLink>
         <NavLink to={`${name ? '/' : '/login'}`} className='nav' >
-          <span onClick={this.handleClick}>{`${ name ? 'Log-out' : 'Log In'}`}
+          <span onClick={this.handleClick}>{`${ name ? 'Log-out' : 'Log-in'}`}
           </span>
         </NavLink>
       </header>
@@ -39,7 +40,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  formType: (text) => dispatch(formType(text)) 
+  formType: (text) => dispatch(formType(text)),
+  logOutUser: () => dispatch(currentUser())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
