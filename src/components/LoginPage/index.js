a91262758dc110e29e0887f9423935c620dd506d
Kelly Zick <kllyzck@gmail.com>
2019-05-09 10:08:09 -0600

    Successfully validate existing user
    Co-authored-by: Nim <nimm.garcia@gmail.com>

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -13,6 +13,30 @@ class LoginPage extends Component {
     }
   }
 
+  validateUser = () => {
+    const { email, password } = this.state;
+    const userInfo = { email, password }
+    const url = 'http://localhost:3000/api/users'
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(userInfo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(result => console.log(result))
+  } 
+
+  handleChange = ({target}) => {
+    this.setState({[target.name]: target.value})
+  } 
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.validateUser();
+  }
+
   render() {
     const header = this.state.existingUser 
       ? 'LOGIN'
@@ -20,12 +44,12 @@ class LoginPage extends Component {
     return (
       <main className='login-page'>
         <h2>{ header }</h2>
-        <form className='login-form'>
+        <form className='login-form' onSubmit={this.handleSubmit}>
           <fieldset className='login-fieldset'>
             <label for='email-input' className='email-input-label input-label'>E-mail</label>
-            <input type='text' id='email-input' className='form-input'></input>
+            <input type='text' id='email-input' className='form-input' onChange={this.handleChange} name='email'></input>
             <label for='password-input' className='password-input-label input-label'>Password</label>
-            <input type='text' id='password-input' className='form-input'></input>
+            <input type='text' id='password-input' className='form-input' onChange={this.handleChange} name='password'></input>
             { !this.state.existingUser && (
               <fieldset>
                 <input type='text' id='confirm-password-input' className='form-input'></input>
