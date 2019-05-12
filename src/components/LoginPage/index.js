a659c4dd999dfcbf6054da21cb01f1353345dbf1
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-12 17:31:00 -0600

    Re-factor loginPage component for readibility

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -18,30 +18,36 @@ class LoginPage extends Component {
     }
   }
 
-  validateUser = async () => {
+  formTypeHandler = () => {
     const { email, password, userName } = this.state;
     const userInfo = this.props.formType === 'login'
     ? ({ email, password })
     : ({ email, password, name: userName });
     const url = this.props.formType === 'login'
-    ? 'http://localhost:3000/api/users'
-    : 'http://localhost:3000/api/users/new';
-    
+      ? 'http://localhost:3000/api/users'
+      : 'http://localhost:3000/api/users/new'
+    return ({ url, userInfo, email, userName})
+  }
+
+  fetchUserFavorites = async (userId) => {
+    const favoritesUrl = `http://localhost:3000/api/users/${userId}/favorites`
+    const favorites = await fetchAnything(favoritesUrl)
+    return favorites.data;
+  }
+
+  submitUserData = async () => {
+    const {url, userInfo, email, userName} = this.formTypeHandler();
     try {
       const response = await postUser(url, userInfo);
-      const favoritesUrl = `http://localhost:3000/api/users/${await response.data.id}/favorites`
-      const favorites = await fetchAnything(favoritesUrl)
-      await this.props.formType === 'login' 
-      ? this.props.signInUser({...response.data, favorites: favorites.data})
-      : this.props.signInUser({ id:response.id, email, name: userName});
+      this.props.formType === 'login' 
+      ? this.props.signInUser({...response.data, favorites: await this.fetchUserFavorites(response.data.id)})
+      : this.props.signInUser({ id:response.id, email, name: userName, favorites: []});
       await this.setState({ error: false});
     } catch(error) {
       this.setState({ error: true })
     }
   }
 
-
-  
   handleChange = ({target}) => {
     this.setState({ [target.name]: target.value })
   } 
@@ -50,44 +56,111 @@ class LoginPage extends Component {
     e.preventDefault();
     const { email, confirmPassword, password, userName } = this.state;
     if (this.props.formType === 'login') {
-      (email.length && password.length) && this.validateUser();
+      (email.length && password.length) && this.submitUserData();
     } else {
-      (email.length && password.length && confirmPassword === password && userName.length) && this.validateUser();
+      (email.length && password.length && confirmPassword === password && userName.length) && this.submitUserData();
     }
   }
   
   render() {
     const { formType, currentUser, changeForm } = this.props
-    const createActLink = <span className='signup-link' onClick={() => changeForm('signup')} >Create Account</span>
-    const header = formType === 'login' 
-      ? 'LOGIN'
-      : 'SIGN-UP';
-    const errorText = formType === 'login'
-      ? <p>Email and password do not match an account, check login info or {createActLink}</p>
-      : 'Email has already been used.'
     if (currentUser.name) return (<Redirect to='/'/>);
+    const signUpInputs = (
+      <div className="signup-form-inputs">
+        <label 
+          htmlFor='userName' 
+          className='sign-up input-label'>
+          Username
+        </label>
+        <input 
+          onChange={this.handleChange} 
+          name="userName" 
+          type='text' 
+          id='userName' 
+          className='sign-up form-input'>
+        </input>
+        <label 
+          htmlFor='email-input' 
+          className='sign-up input-label'>
+          E-mail
+        </label>
+        <input 
+          type='email' 
+          id='email-input' 
+          className='sign-up form-input' 
+          onChange={this.handleChange} 
+          name='email'>
+          </input>
+        <label 
+          htmlFor='password-input' 
+          className='sign-up input-label'>
+          Password
+        </label>
+        <input 
+          type='password' 
+          id='password-input' 
+          className='sign-up form-input' 
+          onChange={this.handleChange} 
+          name='password'>
+        </input>
+        <label 
+          htmlFor='confirm-password-input' 
+          className='sign-up input-label'>
+          Confirm Password
+        </label>
+        <input 
+          onChange={this.handleChange} 
+          name="confirmPassword" 
+          type='password' 
+          id='confirm-password-input' 
+          className='sign-up form-input'>
+        </input>
+      </div>)
+    const signInInputs = (
+      <div className="sign-in-inputs">
+        <label 
+        htmlFor='email-input' 
+        className='email-input-label input-label'>
+        E-mail
+        </label>
+          <input 
+          type='email' 
+          id='email-input' 
+          className='form-input' 
+          onChange={this.handleChange} 
+          name='email' 
+          autoComplete='username'>
+        </input>
+        <label 
+          htmlFor='password-input' 
+          className='password-input-label 
+          input-label'>
+          Password
+        </label>
+        <input 
+          type='password' 
+          id='password-input' 
+          className='form-input' 
+          onChange={this.handleChange} 
+          name='password' 
+          autoComplete='current-password'>
+        </input>
+      </div>)
+    const createActLink = (
+      <span 
+        className='signup-link' 
+        onClick={() => changeForm('signup')}>
+        Create Account
+      </span>)
+    const errorText = formType === 'login'
+    ? <p>Email and password do not match an account, check login info or {createActLink}</p>
+    : 'Email has already been used.'
     return (
       <main className='login-page'>
-        <h2>{ header }</h2> 
+        <h2>{ formType === 'login' ? 'LOGIN' : 'SIGN-UP' }</h2> 
         <form className='login-form' onSubmit={this.handleSubmit}>
           <fieldset className='login-fieldset'>
-            { formType === 'signup' && (
-              <div>
-                <label htmlFor='userName' className='password-input-label input-label'>Username</label>
-                <input onChange={this.handleChange} name="userName" type='text' id='userName' className='form-input'></input>
-              </div>
-            )}
-            <label htmlFor='email-input' className='email-input-label input-label'>E-mail</label>
-            <input type='email' id='email-input' className='form-input' onChange={this.handleChange} name='email' autoComplete='username'></input>
-            <label htmlFor='password-input' className='password-input-label input-label'>Password</label>
-            <input type='password' id='password-input' className='form-input' onChange={this.handleChange} name='password' autoComplete='current-password'></input>
-            { formType === 'signup' && (
-              <div>
-                <label htmlFor='confirm-password-input' className='password-input-label input-label'>Confirm Password</label>
-                <input onChange={this.handleChange} name="confirmPassword" type='password' id='confirm-password-input' className='form-input' autoComplete='new-password'></input>
-              </div>
-            )}
-            {/* REFACTOR create form component to render login and signup in different forms and include new passowrd autocomplete in both password fields */}
+            { formType === 'login' ? signInInputs : signUpInputs }
             { this.state.error && <p className='login-error'>{errorText}</p>}
             <input type="submit" value="Submit" />
           </fieldset>
@@ -103,8 +176,10 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  signInUser: ({id, name, email, favorites}) => dispatch(currentUser(id, name, email, favorites)), 
+  signInUser: ({id, name, email, favorites}) => 
+    dispatch(currentUser(id, name, email, favorites)), 
   changeForm: (text) => dispatch(formType(text))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+
