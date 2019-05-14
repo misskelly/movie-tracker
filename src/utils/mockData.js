d921c2085938703bc00842dc1c279dc2d5dbbaef
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-14 14:29:37 -0600

    Complete testing clean movies

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -84,3 +84,46 @@ export const mockSelectedMovie = {
   vote_count: 1528,
   showFavorites: false
 }
+
+export const mockMoviesArr = [
+  {
+    "vote_count": 5215,
+    "id": 299534,
+    "video": false,
+    "vote_average": 8.5,
+    "title": "Avengers: Endgame",
+    "popularity": 335.962,
+    "poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    "original_language": "en",
+    "original_title": "Avengers: Endgame",
+    "genre_ids": [
+      12,
+      878,
+      28
+    ],
+    "backdrop_path": "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+    "adult": false,
+    "overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
+    "release_date": "2019-04-24"
+  },
+  {
+    "vote_count": 5215,
+    "id": 299534,
+    "video": false,
+    "vote_average": 8.5,
+    "title": "Avengers: Endgame",
+    "popularity": 335.962,
+    "poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    "original_language": "en",
+    "original_title": "Avengers: Endgame",
+    "genre_ids": [
+      12,
+      878,
+      28
+    ],
+    "backdrop_path": "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+    "adult": false,
+    "overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
+    "release_date": "2019-04-24"
+  },
+]
