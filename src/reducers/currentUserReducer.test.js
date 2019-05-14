aafbd4b5d601f1a85b572319bd0651db17d81859
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-14 13:56:01 -0600

    Finish adding tests to currUserReducer

--- src/reducers/currentUserReducer.test.js
+++ src/reducers/currentUserReducer.test.js
@@ -8,10 +8,11 @@ jest.mock('../utils/apiFetches/deleteFavorite')
 jest.mock('../utils/apiFetches/postUser')
 
 describe('currentUserReducer', () => {
-  beforeEach(() => {
-    // jest.mock(mockPostUser = jest.fn()
-    // mockDeleteFavorite = jest.fn()
-  });
+  const mockFav = {
+    movie_id: 299534,
+    user_id: 3,
+    title: "Avengers: Endgame"
+  }
   
   it('should return the initial state', () => {
     const expected = {};
@@ -27,13 +28,9 @@ describe('currentUserReducer', () => {
     
     expect(result).toEqual(expected);
   });
+
   it('should add a movie to a users favorites', () => {
     const {id, name, email, favorites } = mockUser
-    const mockFav = {
-      movie_id: 299534,
-      user_id: 3,
-      title: "Avengers: Endgame"
-    }
     const expected = {
       id,
       name,
@@ -45,39 +42,40 @@ describe('currentUserReducer', () => {
     expect(result).toEqual(expected);
   });
 
-  
-  //TODO: 
-  // mock imported functions
-  // add expect post user thbcw url and new fav
-  //test existing fav, 
-  //add expect deleteFav thbcw state.id and favId
+  it('should add a movie to a users favorites', () => {
+    const {id, name, email } = mockUser
+    const mockInitial = {
+      id,
+      name,
+      email,
+      favorites:[ mockFav ]
+    }
+    const result = currentUserReducer(mockInitial, actions.updateFavorites(mockFav))
+
+    expect(result.favorites).toEqual([]);
+  });
+
+  it('should fetch delete favorite if already favorite', () => {
+    const {id, name, email } = mockUser
+    const mockExisting = {
+      id,
+      name,
+      email,
+      favorites:[mockFav]
+    }
+    currentUserReducer(mockExisting, actions.updateFavorites(mockFav))
+    expect(deleteFavorite).toHaveBeenCalledWith(mockUser.id, mockFav.movie_id)
+  })
+
+  it('should fetch post favorite if not a favorite', () => {
+    const {id, name, email } = mockUser;
+    const mockInitial = {
+      id,
+      name,
+      email,
+      favorites:[]
+    }
+    currentUserReducer(mockInitial, actions.updateFavorites(mockFav))
+    expect(postUser).toHaveBeenCalledWith('http://localhost:3000/api/users/favorites/new', mockFav)
+  })
 });
-
-// beforeEach(() => {
-//   mockUrl = 'someUrl.com';
-//   mockStaff = [{
-//     name: 'christie',
-//     info: 'some info'
-//   }, {
-//     name: 'leta',
-//     info: 'some info'
-//   }];
-//   mockDispatch = jest.fn();
-//   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-//     ok: true,
-//     bio: mockStaff
-//   }));
-//   thunk = fetchStaff(mockUrl);
-// });
-//   it('should return the state with a new todo', () => {
-//         const text = 'learn redux'; 
-//         const id = 1;
-//         const expected = [{ text, id, completed: false }]; 
-        
-//         const result = todosReducer(undefined, actions.addTodo(text, id));
-//         expect(result).toEqual(expected);
-//     });
-// // const expected = [];
-
-// const result = todosReducer(undefined, {});
-// expect(result).toEqual(expected);
